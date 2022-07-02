102bc2009a89096b9b291abf2bdf65c8affcf94b
Todd Rylaarsdam <todd@toddr.org>
2022-07-02 16:45:19 -0500

    init

---
+++ Floatboat/FloatboatApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatboatApp.swift
+//  floatboat
+//
+//  Created by Todd Rylaarsdam on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FloatboatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
